091d4650e36a471a22eb62e2716cf0c99dd35eb5
adamz <adam@zethrae.us>
2026-05-27 00:57:29 +0100

    Fix file-previewer crash

--- file-previewer/Package.swift
+++ file-previewer/Package.swift
@@ -2,6 +2,10 @@
 
 import PackageDescription
 
+let swiftSettings: [SwiftSetting] = [
+  .strictMemorySafety()
+]
+
 let package = Package(
   name: "file-previewer",
   platforms: [
@@ -22,21 +26,24 @@ let package = Package(
       dependencies: [
         .product(name: "SwiftTUI", package: "swift-tui"),
         .product(name: "SwiftTUITerminal", package: "swift-tui"),
-      ]
+      ],
+      swiftSettings: swiftSettings
     ),
     .executableTarget(
       name: "FilePreviewerAppRunner",
       dependencies: [
         "FilePreviewerApp",
         .product(name: "SwiftTUI", package: "swift-tui"),
-      ]
+      ],
+      swiftSettings: swiftSettings
     ),
     .testTarget(
       name: "FilePreviewerAppTests",
       dependencies: [
         "FilePreviewerApp",
         .product(name: "SwiftTUI", package: "swift-tui"),
-      ]
+      ],
+      swiftSettings: swiftSettings
     ),
   ],
   swiftLanguageModes: [.v6]
