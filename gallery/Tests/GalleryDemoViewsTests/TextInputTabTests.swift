27e4bade63e4cd282a8d5e33b446e0fe5bd06b50
adamz <adam@zethrae.us>
2026-05-28 16:59:37 +0100

    test: migrate TextInputTab key assertion to the --tab API
    Completes the --tab migration: this file was edited but missed from the
    staged set in the prior refactor commit, leaving a reference to the
    removed init?(environmentName:). Switch it to init?(key:) like the rest.

--- gallery/Tests/GalleryDemoViewsTests/TextInputTabTests.swift
+++ gallery/Tests/GalleryDemoViewsTests/TextInputTabTests.swift
@@ -29,10 +29,8 @@ struct TextInputTabTests {
     #expect(surface.contains("Multiline editor"))
   }
 
-  @Test("Gallery initial-tab aliases select the text input tab")
-  func galleryInitialTabAliasesIncludeTextInput() throws {
-    #expect(GalleryView.GalleryTab(environmentName: "text-input") == .textInput)
-    #expect(GalleryView.GalleryTab(environmentName: "textinput") == .textInput)
-    #expect(GalleryView.GalleryTab(environmentName: "inputs") == .textInput)
+  @Test("Gallery --tab key selects the text input tab")
+  func galleryTabKeySelectsTextInput() throws {
+    #expect(GalleryView.GalleryTab(key: "text-input") == .textInput)
   }
 }
