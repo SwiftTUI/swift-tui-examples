b765cf0b46cb683912ddce0249f32290bea00bd3
adamz <adam@zethrae.us>
2026-07-25 17:26:29 -0700

    0.3.1

--- gitviz/Package.swift
+++ gitviz/Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     .executable(name: "gitviz", targets: ["GitViz"])
   ],
   dependencies: [
-    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.3.0"),
-    .package(url: "https://github.com/SwiftTUI/swift-tui-charts.git", exact: "0.3.0"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.3.1"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui-charts.git", exact: "0.3.1"),
   ],
   targets: [
     .executableTarget(
